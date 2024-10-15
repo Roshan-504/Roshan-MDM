9ec05ca70cdf82282c4a62d4c27788223c4ab675
Roshan <Roshan@gmail.com>
2024-10-15 21:40:55 +0530

    Initial commit for jenkins with Ant

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        modify();
-    }
-    public static void modify() {
-    System.out.println("Modified method");
-}
-}
